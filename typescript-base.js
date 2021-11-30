0e7e0d0653b86ffc1ca38db76fa4ac03e24dd2a8
Giovanni Mogna <g.mogna@young.business>
2021-11-30 10:24:42 +0100

    feat: separate typescript react/base

---
+++ typescript-base.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ["./rules/base/typescript.js"].map(require.resolve),
+  rules: {},
+};
--- typescript.js
+++ typescript.js
@@ -1,4 +1,15 @@
 module.exports = {
   extends: ["./rules/base/typescript.js"].map(require.resolve),
-  rules: {},
+  settings: {
+    // Append 'ts' extensions to Airbnb 'import/resolver' setting
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx", ".d.ts"],
+      },
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+  },
 };
